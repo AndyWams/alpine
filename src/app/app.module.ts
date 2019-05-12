147b1872ac2b6aef5ac22fab475bfa034de8916f
AndyWams <andrew.wams@gmail.com>
2019-05-12 17:58:15 +0100

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { BannerComponent } from './banner/banner.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+// import { LoadingBarRouterModule } from '@ngx-loading-bar/router';
+import { AppRoutes } from './app.routing';
+import { PageContentComponent } from './core/page-content/page-content.component';
+import { LoginComponent } from './accounts/login/login.component';
+import { SignupComponent } from './accounts/signup/signup.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BannerComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageContentComponent,
+    LoginComponent,
+    SignupComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(AppRoutes),
+    // LoadingBarRouterModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+import { PageContentComponent } from './core/page-content/page-content.component';
+import { BannerComponent } from './banner/banner.component';
+import { LoginComponent } from './accounts/login/login.component';
+import { SignupComponent } from './accounts/signup/signup.component';
+
+export const AppRoutes: Routes = [
+    {
+        path: '',
+        component: PageContentComponent,
+
+        children: [
+            {path: '',
+            component: BannerComponent
+        },
+        {
+            path: 'login',
+            component: LoginComponent
+        },
+        {
+            path: 'signup',
+            component: SignupComponent
+        }
+        ]
+    },
+];
