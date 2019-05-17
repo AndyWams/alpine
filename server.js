c0dc5da7dc588232c2c3ccb2e6a35817bc8f43b8
AndyWams <andrew.wams@gmail.com>
2019-05-17 09:05:57 +0100

    updated server file

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ connection.on('error', function (err) {
 });
 
 app.use('/', appRoute);
-app.use(express.static(__dirname, '/dist'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
