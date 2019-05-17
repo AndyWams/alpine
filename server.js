cd455d0e406070158b33da850935d1887e68f2a7
AndyWams <andrew.wams@gmail.com>
2019-05-17 03:35:56 +0100

    updated server file

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use('/', appRoute);
 app.use(express.static(__dirname, '/dist'));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dis/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 const port = process.env.PORT || 3000;
